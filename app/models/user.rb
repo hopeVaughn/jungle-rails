28aade18cfab2a97ae81e318a26f4f9cd970678c
hopeVaughn <hopevaughnwarren@gmail.com>
2022-10-25 18:06:05 -0700

    finish authentication functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  validates :email, uniqueness: { case_sensitive: false }, presence: true
-  validates :password, presence: true
 
+  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :password, length: {minimum: 3}, presence: true
+
+
+  def self.authenticate_user(email,password)
+    user = User.find_by_email(email.delete(' ').downcase)
+      if user && user.authenticate(password)
+        return user
+      end
+    nil
+  end
+
+  
 end
